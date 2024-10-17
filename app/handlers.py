e4ab6e7f41d98d1baa0930362b3a3a2c0fda31b2
Angel-iii <angelcity0001@gmail.com>
2024-10-17 19:41:53 +0500

    first commit

---
+++ app/handlers.py
@@ -0,0 +1,82 @@
+from aiogram import F, Router
+from aiogram.types import Message, CallbackQuery, InputMediaPhoto
+from aiogram.filters import CommandStart
+import app.keyboards as kb
+
+router = Router()
+group_id = -1002395670506  # ID группы
+
+# Храним выбранные кнопки
+selected_buttons = set()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
+    await message.reply('Как дела?')
+
+
+@router.message(F.text == 'Каталог')
+async def catalog(message: Message):
+    keyboard = kb.create_catalog_keyboard()
+    await message.answer('Выберите категорию товара:', reply_markup=keyboard)
+
+
+@router.callback_query(F.data)
+async def handle_category(callback: CallbackQuery):
+    selected_button = callback.data
+    selected_buttons.add(selected_button)
+
+    await callback.answer('Пожалуйста, загрузите ваше фото.')
+    await callback.message.answer('Отправьте фото')
+
+
+# Храним фотографии в медиагруппе на уровне модуля
+media_groups = {}
+
+
+# Храним фотографии в медиагруппе на уровне модуля
+media_groups = {}
+
+@router.message(F.photo)
+async def handle_photo(message: Message):
+    from main import bot
+
+    # Проверяем, является ли сообщение частью медиагруппы
+    if message.media_group_id:
+        if message.media_group_id not in media_groups:
+            media_groups[message.media_group_id] = []
+
+        media_groups[message.media_group_id].append(InputMediaPhoto(media=message.photo[-1].file_id))
+
+        # Если это последнее сообщение в медиагруппе
+        if len(media_groups[message.media_group_id]) > 1:
+            try:
+                await bot.send_media_group(chat_id=group_id, media=media_groups[message.media_group_id])
+                del media_groups[message.media_group_id]
+
+                # Выводим клавиатуру только один раз после отправки альбома
+                await send_keyboard(message)
+            except Exception as e:
+                await message.answer(f"Ошибка при отправке альбома: {str(e)}")
+
+        else:
+            await message.answer("Группа фото получена.")
+    
+    else:
+        try:
+            photo = message.photo[-1]
+            await bot.send_photo(chat_id=group_id, photo=photo.file_id, caption="Вот ваше фото!")
+
+            # Выводим клавиатуру только один раз, если это одиночное фото
+            await send_keyboard(message)
+        except Exception as e:
+            await message.answer(f"Ошибка при отправке фото: {str(e)}")
+
+async def send_keyboard(message: Message):
+    new_keyboard = kb.create_catalog_keyboard(exclude_buttons=selected_buttons)
+
+    if new_keyboard:
+        await message.answer("Выберите следующую категорию:", reply_markup=new_keyboard)
+    else:
+        await message.answer("Все категории выбраны!")
